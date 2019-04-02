e75e093967a09fe3fd8e04e9d27e9d6098dfe022
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-02 03:59:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function takeANumber(line, name){
-  var length = line.length + 1; 
+  var length = line.length + 1;
   return `Welcome, ${name}. You are number ${length} in line.`
 }
 function nowServing(line) {
   if (line.length == 0){
     return ("There is nobody waiting to be served!")
-  } 
+  }
   else{
-    var temp = line[0] ; 
-    line.shift() ; 
-    return temp ; 
+    var temp = line[0] ;
+    line.shift() ;
+    return temp ;
   }
 }
